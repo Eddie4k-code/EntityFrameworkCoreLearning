8248f3f8ccf44a5c2a0c980ffbc6414a0018f6d6
eddie4k <edwardoc@buffalo.edu>
2024-10-19 18:50:15 -0400

    First commit

---
+++ CodingWiki_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace CodingWiki_DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        
+    }
+}
